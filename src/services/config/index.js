57987a03c36e645fbe546835a61f1a1ee74b8b3c
Gustavo Blanco Ticona <gustavo.blanco@tecsup.edu.pe>
2022-01-30 21:35:33 -0500

    base url

--- src/services/config/index.js
+++ src/services/config/index.js
@@ -1,4 +1,5 @@
-const baseURL = `https://termoconfort-v2.herokuapp.com/`;
+// const baseURL = `https://termoconfort-v2.herokuapp.com/`;
 // const baseURL = `http://localhost:3001/`;
+const baseURL = `https://api.termoconfort.tk/`;
 
 export { baseURL };
